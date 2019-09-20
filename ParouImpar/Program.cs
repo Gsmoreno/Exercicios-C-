e92d62760e158219acbe6d24f2a7f6c943be1607
Gustavo Moreno <gustavosilva.moreno2003@gmail.com>
2019-09-20 17:27:40 -0300

    Exercicios

---
+++ ParouImpar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ParouImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá");
+
+            int nmr = 0;
+            Console.WriteLine("Digite um numero: ");
+            nmr = int.Parse(Console.ReadLine());
+           
+            while(nmr != 0){
+           
+                Console.WriteLine("Digite um numero ou zero para parar: ");
+                nmr = int.Parse(Console.ReadLine());
+            
+            if(nmr % 2 != 0){
+                 Console.WriteLine("Número ímpar");
+            }
+
+            else{
+                Console.WriteLine("Número par.");
+            }
+        
+        
+            }
+        }
+    }
+}
