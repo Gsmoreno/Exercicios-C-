165f70d1184cd8179150a5bdc58f876a489dac0e
Gustavo Moreno <gustavosilva.moreno2003@gmail.com>
2019-09-25 17:19:34 -0300

    uau

---
+++ MatrizImpar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MatrizImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] vetor = {1,2,3,4,5,6};
+           int par = 0;
+           int impar = 0;
+
+
+           
+           
+           
+           Console.WriteLine("Digite os numeros nos vetores ! ");
+           for(int i = 0; i < 6; i++){
+             Console.WriteLine("Vetor["+ i +"]: ");
+             vetor[i] = Convert.ToInt32(Console.ReadLine());
+           }
+            for(int i = 0; i < 6; i++){
+                if(vetor[i] % 2 == 0){
+                    par ++;
+                }
+            }
+            impar = 6 - par;
+            Console.WriteLine("A quantidade de numeros impares é: " + impar);
+            Console.WriteLine("A quantidade de numeros pares é: " + par);
+        }
+    }
+}
---
+++ Triangulos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nmr = 0;
+            int altura = 0;
+            string ast = "*";
+            string ast2 = "*";
+
+            Console.WriteLine("Digite o numero e triangulos: ");
+            nmr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a altura dos triangulos: ");
+            altura = Convert.ToInt32(Console.ReadLine());
+
+        while(nmr != 0){
+            for(int i = 0; i <= altura ; i++){
+               Console.WriteLine(ast2);
+               ast2 += ast;
+             }
+             ast2 ="*";
+             nmr--;
+
+            } 
+        }
+    }
+}
